e63055929671dcd575944160805a0acbad40c27e
Rakhunathan.M <mrakhunathan@gmail.com>
2016-09-07 17:13:18 +0530

    First Application

---
+++ web/js/Application/App1.js
@@ -0,0 +1,8 @@
+var MyFirstApp = angular.module('ModuleName', []);
+MyFirstApp.controller('ModuleNameController', function () {
+    console.log('ModuleNameController..');
+});
+
+MyFirstApp.controller('ModuleNameController2', function () {
+    console.log('ModuleNameController2..');
+});
